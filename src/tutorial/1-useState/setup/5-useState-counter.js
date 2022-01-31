17d7737cc16d5b5e918dc69efa94bd8e5194e491
Rad1m <radim.simanek@protonmail.ch>
2022-01-31 09:22:21 +0000

    more complex counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0)
+  
   const reset = () => {
     setValue(0)
   }
+
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      // setValue(value+1)
+      // this implementation makes sure that if we click button 5 times, then increment will happen 5 times
+      setValue((prevState)=>{
+        return prevState + 1
+      })
+    },2000)
+  }
+
   return (
     <>
     <section style ={{margin: '4rem 0'}}>
@@ -13,6 +25,14 @@ const UseStateCounter = () => {
       <button className='btn' onClick={() => setValue(value-1)}>Decrease</button>
       <button className='btn' onClick={() => setValue(reset)}>Reset</button>
       <button className='btn' onClick={() => setValue(value+1)}>Increase</button>
+    </section>
+
+    <section style ={{margin: '4rem 0'}}>
+      <h2>Increase Counter with delay</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={complexIncrease}>
+        Increase later
+      </button>
     </section>
     </>
   )
