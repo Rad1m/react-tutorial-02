c2338145da719c431e9120931ea29b3ac104e69f
Rad1m <radim.simanek@protonmail.ch>
2022-02-01 19:29:02 +0000

    short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,20 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState('') // change to useState('Radim') to see difference or leave empty useState('')
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
+  
+  return (
+    <>
+       {/* just an example */}
+      {/* <h1>{firstValue}</h1>
+      <h1> value: {secondValue}</h1> */}
+
+      <h1>{text || 'john doe'}</h1>
+
+    </>
+  )
 };
 
 export default ShortCircuit;
