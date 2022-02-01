2e7d680f514e4a6af2c53429742271cf112e123c
Rad1m <radim.simanek@protonmail.ch>
2022-02-01 19:08:47 +0000

    catch url error

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,60 @@
 import React, { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users/QuincyLarson';
+const urlTestError = 'https://api.github.com/users/QuincyLarsonnnn';
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user,setUser] = useState('default user')
+
+  // // if I want to change state I need to use useEffect function as shown below
+  // useEffect(()=>{
+  //   setIsError(true)
+  // })
+
+  useEffect(()=>{
+    fetch(url)
+    //fetch(urlTestError)
+    .then((resp)=>{
+      if (resp.status >= 200 && resp.status <= 299) {
+        return resp.json()
+      }
+      else {
+        setIsLoading(false)
+        setIsError(true)
+        throw new Error (resp.statusText)
+      }
+    })
+    .then((user) => {
+      const {login} = user
+      setUser(login)
+      setIsLoading(false)
+    })
+    .catch((error)=> console.log(error))
+  },[])
+
+  if(isLoading){
+    return (
+       <div>
+         <h1>Loading...</h1>
+       </div>
+    )
+  }
+
+  if(isError){
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    )
+  }
+    
+  return (
+    <div>
+     <h1>{user}</h1>
+    </div>
+  )
 };
 
 export default MultipleReturns;
