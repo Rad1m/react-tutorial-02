818bad94feb8c27e2ad6d6485ea24fb9d03f0c1f
Rad1m <radim.simanek@protonmail.ch>
2022-02-03 08:28:17 +0000

    Ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -7,17 +7,21 @@ const ShortCircuit = () => {
   // const secondValue = text && 'hello world';
 
   const [text, setText] = useState('') // change to useState('Radim') to see difference or leave empty useState('')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
+  const [isError, setIsError] = useState(false)
   
   return (
     <>
        {/* just an example */}
       {/* <h1>{firstValue}</h1>
       <h1> value: {secondValue}</h1> */}
-
+  
       <h1>{text || 'john doe'}</h1>
-
+      <button className='btn' onClick={() => setIsError(!isError)}>Toggle error</button>
+      {isError ? <h1>Error...</h1> : <h1>All good</h1>}
+      {/* If something then "?" else ":" */}
+      {/* ? means then
+      : means else */}
+      {isError ? <p>There is error...</p> : <p>There is no error</p>}
     </>
   )
 };
